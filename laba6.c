daae3c8d035bbbab3fb39a5c32a2b57b22580e3e
xaritosha-xnx <71726367+xaritosha-xnx@users.noreply.github.com>
2020-11-30 20:12:18 +0300

    Update laba6.c

--- laba6.c
+++ laba6.c
@@ -7,10 +7,11 @@
 int main() 
 {
 	setlocale(LC_ALL,"russian");// русский язык
-	int *p1, n, max, mas[2], pnt = 0;
+	int *p1,*p2, n, max, mas[2],j=0,k=0;
 	printf("Введите размер квадратичной матрицы\n");
 	scanf_s("%d", &n); 
-	p1 = (int*)malloc(sizeof(int) * n * n); // инициализация двумерного массива(динамического)
+	p1 = (int*)malloc(sizeof(int) * n * n);
+	p2 = (int*)malloc(sizeof(int) * (n-1) * (n-1));// инициализация двумерного массива(динамического)
 	printf("\nВведите элементы матрицы\n");
 	for (int i = 0; i < n; i++) //ввод элементов
 	{
@@ -29,6 +30,7 @@ int main()
 		}
 		printf("\n");
 	}
+	
 	max = *(p1 + 0 * n + 0); // изначально за макс. по модулю беру 1-е число
 	for (int i = 0; i < n; i++) 
 	{
@@ -42,29 +44,41 @@ int main()
 			}
 		}
 	}
-	printf("Измененная матрица: \n");
-	for (int i = 0; i < n; i++) // вывод измененнной матрицы
+	for (int i = 0; i < n; i++) // замена эл=тов на пересечении на 0 (исп. в качестве триггера)
 	{
-		if (i == mas[0]) 
-		{
-			pnt = 1; 
-		}
 		for (int m = 0; m < n; m++) 
 		{
 			if (i == mas[0] || m == mas[1]) 
 			{
-				continue;
+				*(p1 + i * n + m) = 0;
 			}
-			printf("%2d ", *(p1 + i * n + m));
 		}
-		if (pnt == 1)
-		{ 
-			pnt = 0; 
-		}
-		else
+	}
+	printf("Измененная матрица: \n");
+	for (int i = 0; i < n; i++)// перезаполнение массива
+	{
+		for (int m = 0; m < n; m++)
 		{
-			printf("\n");
+			if (*(p1 + i * n + m) != 0)
+			{
+				*(p2 + j * (n - 1) + k) = *(p1 + i * n + m);
+				k += 1;
+				if (k == (n - 1))
+				{
+					k = 0;
+					j += 1;
+				}
+			}
+		}	
+	}
+	free(p1); // освобождение памяти
+	for (int i = 0; i < (n-1); i++)// вывод матрицы
+	{
+		for (int m = 0; m < (n-1); m++)
+		{
+			printf("%2d", *(p2 + i * (n-1) + m));
 		}
+		printf("\n");
 	}
 	return 0;
 }
